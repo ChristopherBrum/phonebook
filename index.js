5930451776b4570493ab3764d835aa387f648eb0
Christopher Brum <christopher.brum@gmail.com>
2023-07-13 17:08:10 -0700

    modified middleware

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
 const app = express();
+
 const Person = require('./models/person');
 const person = require('./models/person');
 
-app.use(express.json());
 app.use(express.static('build'));
-app.use(cors());
-
+app.use(express.json());
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors());
+
 
 // Routes
 
 app.get('/info', (request, response) => {
-	const infoMessage = `Phonebook has info for ${persons.length} people`;
-	const timeReceived = String(new Date());
-	const htmlText = [infoMessage, timeReceived].map(text => '<p>' + text + '</p>').join('');
-	
-	response.set('Content-Type', 'text/html');
-	response.send(htmlText);
+	Person
+    .find({})
+    .then((persons) => {
+			console.log(persons)
+			const infoMessage = `Phonebook has info for ${persons.length} people`;
+			const timeReceived = String(new Date());
+			const htmlText = [infoMessage, timeReceived].map(text => '<p>' + text + '</p>').join('');
+			response.set('Content-Type', 'text/html');
+			response.send(htmlText);
+    })
+    .catch(err => {
+      console.log('error fetching all persons:', err.message)
+      response.status(404).end()
+    })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -69,9 +79,6 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-	const personId = request.params.id;
-	console.log(personId)
-
 	Person.findByIdAndDelete(request.params.id)
 		.then(person => {
 			response.status(204).end();
